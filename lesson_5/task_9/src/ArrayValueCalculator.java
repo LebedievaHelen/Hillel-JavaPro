61258b910b90e72c922182fd73c54f3211324a1f
LebedievaHelen <115153268+LebedievaHelen@users.noreply.github.com>
2022-10-24 00:50:08 +0300

    Add files via upload

--- lesson_5/task_9/src/ArrayValueCalculator.java
+++ lesson_5/task_9/src/ArrayValueCalculator.java
@@ -4,18 +4,16 @@ import java.util.Objects;
 public class ArrayValueCalculator {
 
     public static int doCalc(String[][] strings) throws ArraySizeException, NumberFormatException, ArrayDataException {
-        if (isCorrectArraySize(strings, 4)) {
+        if (isIncorrectArraySize(strings, 4)) {
             throw new ArraySizeException("array size is not 4X4");
         }
 
         int sum = 0;
-        int[][] numArray = new int[strings.length][strings.length];
 
         for (int i = 0; i < strings.length; i++) {
             for (int j = 0; j < strings[i].length; j++) {
                 try {
-                    numArray[i][j] = Integer.parseInt(strings[i][j]);
-                    sum += numArray[i][j];
+                    sum += Integer.parseInt(strings[i][j]);
                 } catch (NumberFormatException e) {
                     throw new ArrayDataException("array element [" + i + "] [" + j + "] contains symbol or text", e);
                 }
@@ -25,7 +23,7 @@ public class ArrayValueCalculator {
         return sum;
     }
 
-    public static boolean isCorrectArraySize(String[][] array, int arraySize) {
+    public static boolean isIncorrectArraySize(String[][] array, int arraySize) {
         if (array.length != arraySize) {
             return true;
         }
