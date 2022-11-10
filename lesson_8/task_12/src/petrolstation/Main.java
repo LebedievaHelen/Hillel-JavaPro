001bfdd62f444f8b1ae1905ec4d8c57db4338f86
LebedievaHelen <115153268+LebedievaHelen@users.noreply.github.com>
2022-11-10 18:08:17 +0200

    Add files via upload

--- lesson_8/task_12/src/petrolstation/Main.java
+++ lesson_8/task_12/src/petrolstation/Main.java
@@ -3,18 +3,8 @@ package petrolstation;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        PetrolStation station = new PetrolStation(60);
+        PetrolStation petrolStation = new PetrolStation();
 
-        Thread thread1 = new PetrolStation.RefuelThread(station, 20);
-        Thread thread2 = new PetrolStation.RefuelThread(station, 30);
-        Thread thread3 = new PetrolStation.RefuelThread(station, 15);
-
-        thread1.start();
-        thread2.start();
-        thread3.start();
-
-        thread1.join();
-        thread2.join();
-        thread3.join();
+        petrolStation.runThreads();
     }
 }
--- lesson_8/task_12/src/petrolstation/PetrolStation.java
+++ lesson_8/task_12/src/petrolstation/PetrolStation.java
@@ -1,41 +1,73 @@
 package petrolstation;
 
+import java.util.concurrent.Semaphore;
+
 public class PetrolStation {
 
     private static double amount;
+    private final Semaphore semaphore = new Semaphore(3);
 
-    PetrolStation(double amount) {
-        PetrolStation.amount = amount;
+    public PetrolStation() {
+        amount = 100;
     }
 
-    public static class RefuelThread extends Thread {
+    int sleepTime = (int) (Math.random() * (10000 - 3000) + 3000);
 
-        private final PetrolStation station;
+    public void runThreads() throws InterruptedException {
+        RefuelThread thread1 = new RefuelThread("Car1", 30);
+        RefuelThread thread2 = new RefuelThread("Car2", 15);
+        RefuelThread thread3 = new RefuelThread("Car3", 40);
+        RefuelThread thread4 = new RefuelThread("Car4", 10);
+        RefuelThread thread5 = new RefuelThread("Car5", 25);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        thread5.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+        thread4.join();
+        thread5.join();
+    }
+
+    public class RefuelThread extends Thread {
+
         private final double request;
+        private final String name;
 
-        RefuelThread(PetrolStation station, double request) {
-            this.station = station;
+        RefuelThread(String name, double request) {
+            this.name = name;
             this.request = request;
         }
 
         @Override
         public void run() {
             try {
-                sleep((long) (Math.random() * 7000 + 3000));
+                semaphore.acquire();
+                doRefuel(request);
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally {
+                semaphore.release();
             }
-            station.doRefuel(request);
         }
-    }
 
-    synchronized void doRefuel(double request) {
-        System.out.println(Thread.currentThread().getName() + ": available amount of fuel: " + amount);
-        if (amount >= request) {
-            amount -= request;
-            System.out.println(Thread.currentThread().getName() + ": available amount of fuel: " + amount);
-        } else {
-            System.out.println(Thread.currentThread().getName() + ": insufficient amount of fuel");
+        synchronized void doRefuel(double request) throws InterruptedException {
+            boolean isRefuel = false;
+            System.out.println(name + ": Available amount of fuel: " + amount + ". Requested amount of fuel: " + request);
+            if (amount >= request) {
+                amount -= request;
+                isRefuel = true;
+            } else {
+                System.out.println(name + ": Insufficient amount of fuel");
+            }
+            if (isRefuel) {
+                Thread.sleep(sleepTime);
+                System.out.println(name + " Refueling time: " + sleepTime + ". Available amount of fuel after refuel: " + amount);
+            }
         }
     }
 }
